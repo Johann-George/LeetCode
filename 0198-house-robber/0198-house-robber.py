e60f4025b708f88e6c2a87808130da6be6b3c040
Johann George <103820529+Johann-George@users.noreply.github.com>
2025-01-02 23:16:49 +0530

    Time: 0 ms (100%), Space: 17.7 MB (18.75%) - LeetHub

--- 0198-house-robber/0198-house-robber.py
+++ 0198-house-robber/0198-house-robber.py
@@ -1,10 +1,8 @@
 class Solution:
     def rob(self, nums: List[int]) -> int:
-        sum_even,sum_odd=0,0
-        for i in range(len(nums)):
-            if (i%2)==0:
-                sum_even+=nums[i]
-            else:
-                sum_odd+=nums[i]
-        sum_max=max(sum_even,sum_odd)
-        return sum_max
+        rob1,rob2=0,0
+        for num in nums:
+            temp=max(num+rob1,rob2)
+            rob1=rob2
+            rob2=temp
+        return rob2
