8949fe6632fe390bcc41247fbd95ad3dffd175e8
Johann George <103820529+Johann-George@users.noreply.github.com>
2025-08-21 10:53:38 +0530

    Time: 4 ms (11.27%), Space: 42.1 MB (19.85%) - LeetHub

--- 0020-valid-parentheses/0020-valid-parentheses.java
+++ 0020-valid-parentheses/0020-valid-parentheses.java
@@ -1,32 +1,21 @@
 class Solution {
     public boolean isValid(String s) {
-        HashMap<Character, Character> map = new HashMap<>();
-        map.put(')','(');
-        map.put('}','{');
-        map.put(']','[');
         Stack<Character> stack = new Stack<>();
-        char c;
-        char c1;
-        char[] cstr = s.toCharArray();
-        for(int i=0;i<cstr.length;i++){
-            c = cstr[i];
-            if(c==')' && !stack.isEmpty()){
-                c1 = stack.pop();
-                if(c1 != map.get(')')) return false;
+        Map<Character, Character> map = new HashMap<>();
+        map.put(')','(');
+        map.put(']','[');
+        map.put('}','{');
+        for(int i=0;i<s.length();i++){
+            if(map.containsKey(s.charAt(i))){
+                if(!stack.isEmpty() && stack.peek() == map.get(s.charAt(i))){
+                    stack.pop();
+                }
+                else return false;
             }
-            else if(c=='}' && !stack.isEmpty()){
-                c1 = stack.pop();
-                if(c1 != map.get('}')) return false;
+            else{
+                stack.push(s.charAt(i));
             }
-            else if(c==']' && !stack.isEmpty()){
-                c1 = stack.pop();
-                if(c1 != map.get(']')) return false;
-            }
-            else stack.push(c);
         }
-        if(stack.isEmpty()){
-            return true;
-        }
-        return false;
+        return stack.isEmpty();
     }
 }
