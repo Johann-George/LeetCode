d0863c7bb425e958f5dec546b66ac4594feed987
Johann George <103820529+Johann-George@users.noreply.github.com>
2025-09-10 10:36:22 +0530

    Time: 52 ms (19.73%), Space: 101.5 MB (5.36%) - LeetHub

--- 0435-non-overlapping-intervals/0435-non-overlapping-intervals.java
+++ 0435-non-overlapping-intervals/0435-non-overlapping-intervals.java
@@ -5,10 +5,7 @@ class Solution {
             list.add(arr);
         }
         list.sort((a,b)-> a[1]-b[1]);
-        for(int i=0;i<list.size();i++){
-            System.out.println(list.get(i)[0]);
-        }
-        System.out.println(list.size());
+
         List<int[]> newList = new ArrayList<>();
         for(int[] arr1:list){
             if(newList.isEmpty() || arr1[0]>=newList.get(newList.size()-1)[1]){
@@ -16,7 +13,6 @@ class Solution {
             }
             
         }
-        System.out.println(newList.size());
         return (list.size()-newList.size());
     }
 }
