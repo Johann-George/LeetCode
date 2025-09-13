15d6ebd0a19635c5c76036b07f740109b423ae00
Johann George <103820529+Johann-George@users.noreply.github.com>
2025-09-13 11:23:10 +0530

    Time: 8 ms (13.99%), Space: 46.9 MB (35.07%) - LeetHub

---
+++ 0338-counting-bits/0338-counting-bits.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int[] countBits(int n) {
+        int[] ans = new int[n+1];
+        for(int i=1;i<=n;i++){
+            for(int j=0;j<32;j++){
+                if((i & (1<<j))!=0){
+                    ans[i]++;
+                }
+            }
+        }
+        return ans;
+    }
+}
