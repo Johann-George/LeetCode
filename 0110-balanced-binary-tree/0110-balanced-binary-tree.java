0127732920454aa127f7ca4b5defd184fe56ee56
Johann George <103820529+Johann-George@users.noreply.github.com>
2025-10-09 13:07:57 +0530

    Time: 0 ms (100%), Space: 44.3 MB (87.2%) - LeetHub

--- 0110-balanced-binary-tree/0110-balanced-binary-tree.java
+++ 0110-balanced-binary-tree/0110-balanced-binary-tree.java
@@ -15,18 +15,28 @@
  */
 class Solution {
     public boolean isBalanced(TreeNode root) {
-        return dfs(root)[0] == 1;
+        int height = dfs(root);
+        if(height == -1){
+            return false;
+        }
+        return true;
     }
 
-    public int[] dfs(TreeNode root){
+    public int dfs(TreeNode root){
         if(root == null){
-            return new int[]{1,0};
+            return 0;
         }
-        int[] left = dfs(root.left);
-        int[] right = dfs(root.right);
+        
+        int left = dfs(root.left);
+        int right = dfs(root.right);
 
-        boolean balanced = (left[0] == 1 && right[0] == 1) && (Math.abs(left[1]-right[1])<=1);
-        int height = 1 + Math.max(left[1],right[1]);
-        return new int[]{balanced? 1:0, height};
+        if(left == -1 || right == -1){
+            return -1;
+        }
+        else if(Math.abs(left-right)>1){
+            return -1;
+        }
+
+        return 1 + Math.max(left, right);
     }
 }
